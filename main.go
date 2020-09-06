f560e4770411681245a50a641a20d3bebad8e1ac
genndy007 <kochevgenazp@gmail.com>
2020-09-06 18:36:31 +0300

    Simplified code for reading

--- main.go
+++ main.go
@@ -3,5 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hope this bot works")
+	updates, err := GetUpdates()
+	PrintError(err)
+	numUpdates := len(updates)
+	for {
+		updates, err = GetUpdates()
+		PrintError(err)
+		if len(updates) > numUpdates {
+			for i := numUpdates; i < len(updates); i++ {
+				// react to new msgs
+				msgTxt := updates[i].Message.Text
+				chatId := updates[i].Message.Chat.Id
+				if msgTxt == "/start" {
+					err = SendMessage(chatId, "Hello, it's bot for playing")
+					PrintError(err)
+				}
+				if msgTxt == "/rules" {
+					err = SendMessage(chatId, "Here are the rules")
+					PrintError(err)
+				}
+			}
+		}
+		numUpdates = len(updates)
+	}
+}
+
+func PrintError(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
 }
