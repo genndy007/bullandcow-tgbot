e5d7440b02e85a28948f1b4c57f2e2364488eb10
genndy007 <kochevgenazp@gmail.com>
2020-09-06 20:10:57 +0300

    Added gameplay methods and checking bulls and cows amount

---
+++ gamemethods.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+func GenerateNumber() int {
+	// Very clever random number implementation :)
+	result := 0
+	rand.Seed(time.Now().UnixNano())
+	allNumbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
+	plier := 1000
+	for time := 0; time < 4; time++ {
+		border := len(allNumbers)
+		if time == 0 {
+			border--
+		}
+		i := rand.Intn(border)
+		result += allNumbers[i] * plier
+		allNumbers = RemoveFromSlice(allNumbers, i)
+		plier /= 10
+	}
+	return result
+
+}
+func RemoveFromSlice(slice []int, s int) []int {
+	return append(slice[:s], slice[s+1:]...)
+}
+
+func IsInSlice(slice []rune, val rune) bool {
+	for _, item := range slice {
+		if item == val {
+			return true
+		}
+	}
+	return false
+}
+
+func CheckBullsAndCows(guess, number string) [2]int {
+	bullsAndCows := [2]int{0, 0}
+	for i := 0; i < 4; i++ {
+		if guess[i] == number[i] {
+			bullsAndCows[0]++
+		} else if IsInSlice([]rune(number), rune(guess[i])) {
+			bullsAndCows[1]++
+		}
+	}
+
+	return bullsAndCows
+}
