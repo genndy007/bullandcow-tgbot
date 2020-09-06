638ad12a24642c0a5097e0ba12b0eab1f570ec17
genndy007 <kochevgenazp@gmail.com>
2020-09-06 15:11:48 +0300

    Added smth like all structures I need such as update and send results

--- tgentities.go
+++ tgentities.go
@@ -1,5 +1,16 @@
 package main
 
+// Telegram Updates Result
+type UpdatesResultT struct {
+	Ok     bool      `json:"ok"`
+	Result []UpdateT `json:"result"`
+}
+
+type SendResultT struct {
+	Ok     bool     `json:"ok"`
+	Result MessageT `json:"result"`
+}
+
 // Telegram Update
 type UpdateT struct {
 	UpdateId int      `json:"update_id"`
@@ -8,9 +19,10 @@ type UpdateT struct {
 
 // Telegram Message
 type MessageT struct {
-	MessageId int   `json:"message_id"`
-	Chat      ChatT `json:"chat"`
-	Date      int   `json:"date"`
+	MessageId int    `json:"message_id"`
+	Chat      ChatT  `json:"chat"`
+	Date      int    `json:"date"`
+	Text      string `json:"text"`
 }
 
 // Telegram Chat
