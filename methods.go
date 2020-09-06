bd2c7102d307def35872dd2025314991d2df1f55
genndy007 <kochevgenazp@gmail.com>
2020-09-06 16:51:08 +0300

    Now my program is able to check bot updates and send its own messages

---
+++ methods.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+)
+
+var baseUrl = "https://api.telegram.org/bot"
+
+func GetBody(methodName string) ([]byte, error) {
+	resp, err := http.Get(baseUrl + botToken + methodName)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
+// Method getUpdates receives incoming updates
+func GetUpdates() ([]UpdateT, error) {
+	methodName := "/getUpdates"
+	body, err := GetBody(methodName)
+	if err != nil {
+		return nil, err
+	}
+
+	var updatesResult UpdatesResultT
+	json.Unmarshal(body, &updatesResult)
+
+	return updatesResult.Result, nil
+
+}
+
+// Send text message to chat with chat_id
+func SendMessage(chatId int, text string) error {
+	methodName := "/sendMessage"
+	query := methodName + "?chat_id=" + strconv.Itoa(chatId) + "&text=" + text
+	fmt.Println(query)
+	body, err := GetBody(query)
+	if err != nil {
+		return err
+	}
+	var sendResult SendResultT
+	err = json.Unmarshal(body, &sendResult)
+
+	if !sendResult.Ok {
+		return errors.New("Message is not sent correctly")
+	}
+
+	return nil
+}
